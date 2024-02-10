7fb761aca70b26855aabcf8e9947d7e8924a348a
Viktoriia <nazaruk.v92@gmail.com>
2024-02-10 16:38:29 +0200

    fix bubbles

--- src/constants/contacts.const.ts
+++ src/constants/contacts.const.ts
@@ -1,15 +1,18 @@
 export interface IContact {
   value: string;
   href: string;
+  label: string;
 }
 
 export const contacts: IContact[] = [
   {
     value: 'info@movemnt.digital',
     href: 'mailto:info@movemnt.digital',
+    label: 'Email',
   },
   {
-    value: 'Instagram',
+    value: 'Movemnt.digital',
     href: 'https://www.instagram.com/movemnt.digital/',
+    label: 'Instagram',
   },
 ];
