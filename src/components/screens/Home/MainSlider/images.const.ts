c7d61b403becab3a6a824bbbe56aa0e6d022580c
Viktoriia <nazaruk.v92@gmail.com>
2024-02-10 22:25:32 +0200

    Update main content and order for first slide on production

--- src/components/screens/Home/MainSlider/images.const.ts
+++ src/components/screens/Home/MainSlider/images.const.ts
@@ -6,17 +6,12 @@ import img14 from '~/assets/img/new-content/main/3-new.jpeg';
 
 // guy with towel
 import img19 from '~/assets/img/new-content/production/6/1.jpg';
-import img20 from '~/assets/img/new-content/production/6/2.jpg';
-import img21 from '~/assets/img/new-content/production/6/3.jpg';
 
 // selena
 import img22 from '~/assets/img/new-content/production/12/1.jpg';
-import img23 from '~/assets/img/new-content/production/12/2.jpg';
-import img24 from '~/assets/img/new-content/production/12/3.jpg';
 
 // glasses
 import img25 from '~/assets/img/new-content/production/3/1.jpg';
-import img26 from '~/assets/img/new-content/production/3/2.jpg';
 
 const images: IImg[][] = [
   [
@@ -41,33 +36,13 @@ const images: IImg[][] = [
       alt: '',
     },
     {
-      id: 20,
-      src: img20,
-      alt: '',
-    },
-    {
-      id: 21,
-      src: img21,
-      alt: '',
-    }, {
       id: 22,
       src: img22,
       alt: '',
-    }, {
-      id: 23,
-      src: img23,
-      alt: '',
-    }, {
-      id: 24,
-      src: img24,
-      alt: '',
-    }, {
+    },
+    {
       id: 25,
       src: img25,
-      alt: '',
-    }, {
-      id: 26,
-      src: img26,
       alt: '',
     },
 
