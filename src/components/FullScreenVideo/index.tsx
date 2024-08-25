16d818ce04fdeb7e116deecb6cea49d767775405
vink92 <nazaruk.v92@gmail.com>
2024-08-25 16:05:10 +0300

    add test video

---
+++ src/components/FullScreenVideo/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const FullScreenVideo = ({ src }: { src: string }) => (
+  <div style={ {
+    position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', overflow: 'hidden', zIndex: -1,
+  } }
+  >
+    <video
+      src={ src }
+      autoPlay
+      muted
+      loop
+      style={ {
+        position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover',
+      } }
+    />
+  </div>
+);
+
+export default FullScreenVideo;
--- src/components/screens/Home/Home.tsx
+++ src/components/screens/Home/Home.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react';
 import Layout from '~/components/layout/Layout';
-import MainSlider from './MainSlider/MainSlider';
+
+// import MainSlider from './MainSlider/MainSlider';
+import FullScreenVideo from '~/components/FullScreenVideo';
 import DesktopCards from './Cards/Cards';
 
 const Home: FC = () => (
   <Layout title="Home" className="home">
-    <MainSlider />
+    {/* <MainSlider /> */}
+    <FullScreenVideo src="/move_test.mp4" />
     <DesktopCards />
   </Layout>
 );
