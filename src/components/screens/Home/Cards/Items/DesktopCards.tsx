df3092084c9b0bc653f743385c554a07019626b6
Viktoriia Nazaruk <nazaruk.v92@gmail.com>
2024-09-12 08:19:36 +0300

    fix wording for bubbles

--- src/components/screens/Home/Cards/Items/DesktopCards.tsx
+++ src/components/screens/Home/Cards/Items/DesktopCards.tsx
@@ -43,7 +43,7 @@ const DesktopCards: FC = () => {
       >
         {/* <Image src={ firstBubble } alt="firstBubble" /> */}
         <p className="card__contacts_inline">
-          <span style={ { marginLeft: 50 } }>Movemnt.digital</span>
+          <span style={ { marginLeft: 50 } }>Movemnt.digital </span>
           is a bicoastal creative web and production studio with offices in Los Angeles and New York.
           <br />
           We offer bespoke brand strategies, user-centric websites, and tasteful storytelling creatives to elevate multi-platform brand channels.
@@ -59,7 +59,7 @@ const DesktopCards: FC = () => {
         {/* <Image src={ secondBubble } alt="secondBubble" /> */}
         <p>
           <span style={ { marginRight: 50 } }>Services</span>
-          Studio: Creative strategy, Brand identity, web design,
+          Studio: Creative strategy, Brand identity, Web design,
           <br />
           Web development, Technical support.
           <br />
