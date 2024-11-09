85f32fd51bbf73593a43cde321a95458725bd7e4
Viktoriia Nazaruk <nazaruk.v92@gmail.com>
2024-11-09 22:44:08 +0200

    Fix audio play on mob

--- src/components/FullScreenVideo/index.tsx
+++ src/components/FullScreenVideo/index.tsx
@@ -10,6 +10,7 @@ const FullScreenVideo = ({ src }: { src: string }) => (
       autoPlay
       muted
       loop
+      playsInline
       style={ {
         position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover',
       } }
